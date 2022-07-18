b55ed93e44efcfd3e1cdedee316ab370dd2e2c44
yazan-nidal <yazan.nidal@hotmail.com>
2022-07-18 14:28:35 +0300

    add book mode,their controller,& their test ,update II

--- src/main/java/exp/exalt/bookshop/controller/BookController.java
+++ src/main/java/exp/exalt/bookshop/controller/BookController.java
@@ -3,8 +3,7 @@ package exp.exalt.bookshop.controller;
 import exp.exalt.bookshop.model.Book;
 import exp.exalt.bookshop.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,24 @@ public class BookController {
         return bookService.getAllBooks();
     }
 
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable("id") long id) {
+        return bookService.getBook(id);
+    }
+
+    @PostMapping(value = "/books")
+    public void addBook(@RequestBody Book book){
+        bookService.addBook(book);
+    }
+
+    @PutMapping(value = "/books/{id}")
+    public void updateBook(@RequestBody Book book, @PathVariable("id") long id){
+        bookService.updateBook(book, id);
+    }
+
+    @DeleteMapping(value = "/books/{id}")
+    public void deleteBook(@PathVariable("id") long id){
+        bookService.deleteBook(id);
+    }
 
 }
--- src/main/java/exp/exalt/bookshop/service/BookService.java
+++ src/main/java/exp/exalt/bookshop/service/BookService.java
@@ -3,19 +3,42 @@ package exp.exalt.bookshop.service;
 import exp.exalt.bookshop.model.Book;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class BookService {
 
-    private List<Book> books = Arrays.asList(
+    private List<Book> books = new ArrayList<>(Arrays.asList(
             new Book(110000,"Code Complete",111000,112000,0),
             new Book(110001,"Clean Code",111001,112001,1),
             new Book(110001,"The Little Schemer",111002,112002,2)
-    );
+    ));
 
     public List<Book> getAllBooks() {
         return books;
+    }
+
+    public Book getBook(long id) {
+        return books.stream().filter(b -> b.getId() == id).findFirst().orElse(null);
+    }
+
+    public void addBook(Book book){
+        books.add(book);
+    }
+
+    public void updateBook(Book book, long id){
+        int i = 0;
+        for (Book bookI:books)
+        {
+            if(bookI.getId() == id) {
+                books.set(i,book);
+                return;
+            }
+            i++;
+        }
+    }
+
+    public void deleteBook(long id) {
+        books.removeIf(b -> b.getId() == id);
     }
 }
