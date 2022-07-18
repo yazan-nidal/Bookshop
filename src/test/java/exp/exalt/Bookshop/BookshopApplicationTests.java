892c5dd354a11ca8b16cc2b5aeab9ee54dc4a80b
yazan-nidal <yazan.nidal@hotmail.com>
2022-07-18 08:47:11 +0300

    Initial commit

---
+++ src/test/java/exp/exalt/Bookshop/BookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package exp.exalt.Bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
